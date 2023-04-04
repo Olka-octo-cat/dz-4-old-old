a73d987e25177394a7aefd9da2c2399eb5fd97de
Olka-octo-cat <olga.khoroshok@gmail.com>
2023-04-04 20:31:01 +0300

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/*
+ * My First Java program to say Hello
+ */
+public class Hello {   // Save as "Hello.java" under "~/myProject"
+   public static void main(String[] args) {
+      System.out.println("Hello, world from Mac!");
+   }
+}
+
+
+
